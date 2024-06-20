edc71c24ca7e9c56738a1b5d1f1f9efab9f7adbb
Daniel Ribeiro <143565521+DaniellRibeiiro@users.noreply.github.com>
2024-06-19 23:09:44 -0300

    Add files via upload
    rotas get id e get funcionais com o banco

---
+++ api-nodejs-alfa/src/database/knex/index.ts
@@ -0,0 +1,16 @@
+import { Knex, knex } from "knex";
+
+const config: Knex.Config = {
+  client: "mysql2",
+  connection: {
+    host: "127.0.0.1",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "vacinet",
+  },
+};
+
+const knexInstance = knex(config);
+
+export default knexInstance;
